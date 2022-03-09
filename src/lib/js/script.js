725951c651a4c87f1ccb8187496621bf3b7b6397
Garnet3106 <manage@gant.work>
2022-03-09 16:37:42 +0900

    add: script

---
+++ src/lib/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let msg = '\
+`《￣＞―――＜￣》\n\
+　ﾋ／=-　　　-=＼ｿヽ\n\
+　/ /ﾉメｲ人ﾚ∠ﾙヽヽ|\n\
+　ﾚY|(ﾋO　　ﾋO)|| ||\n\
+　 目 ﾞﾞヽフ ﾞﾞ目)||\n\
+　 |人　　　　 || リ\n\
+　 |ﾉﾙ＞ー―-＜リﾙﾉ\n\
+';
+
+function onProductItemClick(_event) {
+    alert(msg);
+}
